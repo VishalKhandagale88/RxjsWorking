3e72743628dcf9c137167f654ef76daa162e6cce
vishal <vishal88kh@gmail.com>
2023-06-04 22:10:41 +0530

    added routing for observable

--- RxJsDemo/src/app/app-routing.module.ts
+++ RxJsDemo/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PromiseComponent } from './promise/promise.component';
 import { PromiseImplementationComponent } from './promise-implementation/promise-implementation.component';
+import { ObservableComponent } from './observable/observable.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,11 @@ const routes: Routes = [
   {
     path:"**",
     component:PromiseImplementationComponent
+  },{
+    path:"observable",
+    component:ObservableComponent
   }
+
 ];
 
 @NgModule({
